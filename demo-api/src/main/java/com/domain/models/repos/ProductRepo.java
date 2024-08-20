5b14b7508dca974ebe33421267ec80a0ed23e699
MuhammadAnbiya23 <anbiya17agustus@gmail.com>
2024-08-20 13:55:06 +0700

    update 20/8 - create custom query

--- demo-api/src/main/java/com/domain/models/repos/ProductRepo.java
+++ demo-api/src/main/java/com/domain/models/repos/ProductRepo.java
@@ -3,10 +3,16 @@ package com.domain.models.repos;
 import java.util.List;
 
 import com.domain.models.entities.Product;
+
+import jakarta.websocket.server.PathParam;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepo extends CrudRepository<Product, Long> { // menggunakan framework CRUDRepository ada juga JPA Repository
-    
-    List<Product> findByNameContains(String name); // salah satu contoh custom query 
-    
+public interface ProductRepo extends CrudRepository<Product, Long> { // menggunakan framework CRUDRepository ada juga
+                                                                     // JPA Repository
+
+    @Query("SELECT p FROM Product p WHERE p.name= :param")
+    List<Product> findByNameContains(@PathParam("name") String name); // salah satu contoh custom query
+
 }

50928db17fb4586428d35cdc8a113d11131c5c7b
MuhammadAnbiya23 <anbiya17agustus@gmail.com>
2024-08-24 16:45:18 +0700

    update 24/8 - create web security config

---
+++ demo-api/src/main/java/com/domain/security/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.domain.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    private AppUserService appUserService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    
+
+}

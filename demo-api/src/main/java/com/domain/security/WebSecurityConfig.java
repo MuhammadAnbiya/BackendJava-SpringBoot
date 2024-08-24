8f83d37dd6ae7d9c3f7e00c253a2ea27841ed0ba
MuhammadAnbiya23 <anbiya17agustus@gmail.com>
2024-08-24 17:21:10 +0700

    update 24/8 - create AppUserController

--- demo-api/src/main/java/com/domain/security/WebSecurityConfig.java
+++ demo-api/src/main/java/com/domain/security/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package com.domain.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +18,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    
+    @Override
+    protected void configure(HttpSecurity auth) throws exception {
+        http.csrf().disable()
+        .authorizeRequest().antMatchers("api/users/register").permitAll()
+        .anyRequest().fullyAuthenticated()
+        .and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+        
+    }
 
 }

559313dccecdbccc9b8b47d63c1fc224e0c9434e
muhammadanbiya <muhammadanbiya@localhost.localdomain>
2024-12-02 14:32:52 +0700

    update - create a new file

---
+++ demoauthgoogle/src/main/java/com/googleauth/demoauthgoogle/DemoauthgoogleApplication.java
@@ -0,0 +1,13 @@
+package com.googleauth.demoauthgoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoauthgoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoauthgoogleApplication.class, args);
+	}
+
+}
---
+++ demoauthgoogle/src/test/java/com/googleauth/demoauthgoogle/DemoauthgoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googleauth.demoauthgoogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoauthgoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

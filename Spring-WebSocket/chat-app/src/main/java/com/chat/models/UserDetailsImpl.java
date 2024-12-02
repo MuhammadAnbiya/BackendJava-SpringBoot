6c44f19580bc6d1cdc6ff666c17d32445865a2f7
muhammadanbiya <muhammadanbiya@localhost.localdomain>
2024-12-02 13:45:22 +0700

    update - resolved client-id prob

--- Spring-WebSocket/chat-app/src/main/java/com/chat/models/UserDetailsImpl.java
+++ Spring-WebSocket/chat-app/src/main/java/com/chat/models/UserDetailsImpl.java
@@ -1,26 +1,47 @@
-package com.chat.models;
+// package com.chat.models;
 
-import java.util.Collection;
+// import java.util.Collection;
+// import java.util.Collections;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.GrantedAuthoritiesContainer;
-import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.GrantedAuthority;
+// import org.springframework.security.core.authority.GrantedAuthoritiesContainer;
+// import org.springframework.security.core.authority.SimpleGrantedAuthority;
+// import org.springframework.security.core.userdetails.UserDetails;
 
-import com.chat.models.entities.User;
+// import com.chat.models.entities.User;
 
-public class UserDetailsImpl implements UserDetails{
+// public class UserDetailsImpl implements UserDetails{
     
-    private final User user;
+//     private final User user;
 
-    public UserDetailsImpl(User user){
-        this.user = user;
-    }
+//     public UserDetailsImpl(User user){
+//         this.user = user;
+//     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> (){
+//     @Override
+//     public Collection<? extends GrantedAuthority> grantedAuthorities() (){
+//         return Collections.singletonList(new SimpleGrantedAuthority(user.getAuthority()));
+//     }
 
-    }
+//     @Override
+//     public String getPassword() {
+//         return user.getPassword();
+//     }
 
+//     @Override
+//     public String getUsername(){
+//         return user.getUsername();
+//     }
+
+//     @Override
+//     public boolean isAccountNonExpired(){
+//         return true;
+//     }
+
+//     @Override
+//     public boolean isAccountNonLocked() {
+//         return true;
+//     }
     
     
-}
+// }
--- Spring-WebSocket/chat-app/src/main/java/com/chat/services/UserDetailsServiceImpl.java
+++ Spring-WebSocket/chat-app/src/main/java/com/chat/services/UserDetailsServiceImpl.java
@@ -1,24 +1,24 @@
-package com.chat.services;
+// package com.chat.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.core.userdetails.UsernameNotFoundException;
+// import org.springframework.stereotype.Service;
 
-import com.chat.models.entities.User;
+// import com.chat.models.entities.User;
 
-@Service
-public class UserDetailsServiceImpl implements UserDetailsService{
+// @Service
+// public class UserDetailsServiceImpl implements UserDetailsService{
     
-    @Autowired
-    private UserService userService;
+//     @Autowired
+//     private UserService userService;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+//     @Override
+//     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
 
-        User user =  userService.findByUsername(username);
-        return new UserDetailsImpl(user);
+//         User user =  userService.findByUsername(username);
+//         return new UserDetailsImpl(user);
 
-    }
-}
+//     }
+// }
